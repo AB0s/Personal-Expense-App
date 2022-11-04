4cf198511be2cbfc21e1bed9cb7103884ea3b848
AB0s <93539709+AB0s@users.noreply.github.com>
2022-11-04 23:52:18 +0600

    Button

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -7,29 +7,29 @@ import '../models/transaction.dart';
 class TransactionList extends StatelessWidget {
   final List<Transaction> transactions;
   final Function deleteTx;
-  const TransactionList(this.transactions,this.deleteTx);
+  const TransactionList(this.transactions, this.deleteTx);
 
   @override
   Widget build(BuildContext context) {
     return transactions.isEmpty
-        ? LayoutBuilder(builder: (ctx,constraints){
-          return Column(
-            children: <Widget>[
-              const Text(
-                'No transactions added yet!',
-              ),
-              const SizedBox(
-                height: 20,
-              ),
-              SizedBox(
-                  height: constraints.maxHeight*0.6,
-                  child: Image.asset(
-                    'assets/images/waiting.png',
-                    fit: BoxFit.cover,
-                  )),
-            ],
-          );
-    })
+        ? LayoutBuilder(builder: (ctx, constraints) {
+            return Column(
+              children: <Widget>[
+                const Text(
+                  'No transactions added yet!',
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                SizedBox(
+                    height: constraints.maxHeight * 0.6,
+                    child: Image.asset(
+                      'assets/images/waiting.png',
+                      fit: BoxFit.cover,
+                    )),
+              ],
+            );
+          })
         : ListView.builder(
             itemBuilder: (ctx, index) {
               return Card(
@@ -50,11 +50,18 @@ class TransactionList extends StatelessWidget {
                   subtitle: Text(
                     DateFormat.yMMMd().format(transactions[index].date),
                   ),
-                  trailing: IconButton(
-                    icon: Icon(Icons.delete),
-                    color: Colors.red,
-                    onPressed: ()=>deleteTx(transactions[index].id),
-                  ),
+                  trailing: MediaQuery.of(context).size.width > 460
+                      ? FlatButton.icon(
+                          onPressed: ()=>deleteTx(transactions[index].id),
+                          icon: Icon(Icons.delete),
+                          label: Text('Delete'),
+                          textColor: Theme.of(context).errorColor,
+                        )
+                      : IconButton(
+                          icon: Icon(Icons.delete),
+                          color: Colors.red,
+                          onPressed: () => deleteTx(transactions[index].id),
+                        ),
                 ),
               );
             },
